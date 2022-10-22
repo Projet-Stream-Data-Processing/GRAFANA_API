f3fca57022311e0aa08c7b20805c986ccc4dde3f
“tellebma” <pdmtc.bellet@gmail.com>
2022-10-22 22:54:59 +0200

    UPADE API, fix GRAFANA LINK

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
-from mongodb import mongodb_stations_infos, mongodb_stations_bikes
+from mongodb import mongodb_stations_infos, mongodb_stations_bikes, stations
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def main():
     return "<h1>ca marche !</h1>regarde la doc mnt. <b>#y a pas de doc.</b></br></br><h1>Documentation</h1>Infos sur les stations : <a href='./api/stations_infos'>/api/stations_infos</a></br>Vélo dispo dans chaque stations : <a href='./api/stations_bikes'>/api/stations_bikes</a>"
+
+
+@app.route("/search", methods=['POST'])
+def search():
+    return jsonify(["available_bike_stands","available_bikes","all_bike_stands","all_available_bikes","id"])
+
+@app.route("/query", methods=['POST'])
+def query():
+    req = request.get_json()
+    data = [
+        {
+            "target": req['targets'][0]['target'],
+            "datapoints": stations(req['targets'][0]['target']) 
+        }
+    ]
+    return jsonify(data)
+
+@app.route("/annotations")
+def annotations():
+    req = request.get_json()
+    data = [
+        {
+            "annotation": 'This is the annotation',
+            "time": (convert_to_time_ms(req['range']['from']) +
+                     convert_to_time_ms(req['range']['to'])) / 2,
+            "title": 'Deployment notes',
+            "tags": ['tag1', 'tag2'],
+            "text": 'Hm, something went wrong...'
+        }
+    ]
+    return jsonify(data)
+
 
 
 @app.route('/api/stations_infos')
@@ -19,7 +52,6 @@ def get_stations_info():
 @app.route('/api/stations_bikes')
 def get_stations_bikes():
     data = mongodb_stations_bikes()
-    print(data)
     return jsonify(data)
 
 if __name__ == "__main__":
--- mongodb.py
+++ mongodb.py
@@ -1,11 +1,17 @@
 from pymongo import MongoClient
-
+from calendar import timegm
+from datetime import datetime
 mongoUser="user"
 mongoPass="password"
 mongoClient = MongoClient(f"mongodb://{mongoUser}:{mongoPass}@localhost:27017")
 db=mongoClient.Velov
 c_StationName=db.StationName
 c_Station=db.Station
+
+
+def convert_to_time_ms(timestamp):
+    return 1000 * timegm(datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S').timetuple())
+
 
 def mongodb_stations_infos():
     return [e for e in c_StationName.find({},{"_id":0})]
@@ -17,3 +23,27 @@ def mongodb_stations_bikes():
 
 
 
+def stations(recherche:str):
+    r = []
+    if recherche == "all_bike_stands" or recherche == "all_available_bikes":
+        if recherche == "all_bike_stands":
+            recherche_ = "available_bike_stands"
+        elif recherche == "all_available_bikes":
+            recherche_ = "available_bikes"
+
+        d = {}
+        for e in c_Station.find({},{"_id":0}):
+            if not convert_to_time_ms(e['timestamp']) in d:
+                d[convert_to_time_ms(e['timestamp'])] = int(e[recherche_])
+            else:
+                d[convert_to_time_ms(e['timestamp'])] += int(e[recherche_])
+        for k,v in d.items():
+            r.append([v,k])
+        return r
+        
+        
+    
+    for e in c_Station.find({},{"_id":0}):
+        r.append([e[recherche],convert_to_time_ms(e['timestamp'])])
+    
+    return r
