26e225e60bd1df87fc9e34e3ff8982b8908401bb
“tellebma” <pdmtc.bellet@gmail.com>
2022-10-22 15:12:59 +0200

    fix #1

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 from flask import Flask, render_template, jsonify
 
-from mongodb import get_stations_infos, get_stations_bikes
+from mongodb import mongodb_stations_infos, mongodb_stations_bikes
 
 app = Flask(__name__)
 
 
-
-
 @app.route('/')
 def main():
-    return "ca marche ! regarde la doc mnt."
+    return "<h1>ca marche !</h1>regarde la doc mnt. <b>#y a pas de doc.</b></br></br><h1>Documentation</h1>Infos sur les stations : <a href='./api/stations_infos'>/api/stations_infos</a></br>Vélo dispo dans chaque stations : <a href='./api/stations_bikes'>/api/stations_bikes</a>"
 
 
-@app.route('/api/stations_info')
+@app.route('/api/stations_infos')
 def get_stations_info():
-    data = get_stations_infos()
-    return jsonify({"lignes":data})
+    data = mongodb_stations_infos()
+    return jsonify(data)
 
 
 @app.route('/api/stations_bikes')
 def get_stations_bikes():
-    data = get_stations_bikes()
-    return "test"
+    data = mongodb_stations_bikes()
+    print(data)
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8888)
--- mongodb.py
+++ mongodb.py
@@ -7,18 +7,13 @@ db=mongoClient.Velov
 c_StationName=db.StationName
 c_Station=db.Station
 
-def get_stations_infos():
-    return [e for e in c_StationName.find({})]
+def mongodb_stations_infos():
+    return [e for e in c_StationName.find({},{"_id":0})]
 
 
 
-def get_stations_bikes():
-    a = [e for e in c_Station.find({})]
-    return a[:100]
+def mongodb_stations_bikes():
+    return [e for e in c_Station.find({},{"_id":0})]
 
 
-if __name__ == "__main__":
-    data = get_stations_bikes()
-    print(data)
-    
 
