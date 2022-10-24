75ef135aac3bb5bead5d0b53b19b92fad82c3a53
Maxime <66943979+tellebma@users.noreply.github.com>
2022-10-24 08:37:41 +0200

    change DEBUG MODE

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ def get_stations_bikes():
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8888)
+    app.run(debug=False, host="0.0.0.0", port=8888)
